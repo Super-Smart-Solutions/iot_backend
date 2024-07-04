3952ebffd0b264755baabc3824084fa877d73c79
ahmedelgamal0 <a.elgamal98@outlook.com>
2024-07-04 13:57:27 +0300

    remove dummy

--- iot_backend/web/api/dummy/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Dummy model API."""
-from iot_backend.web.api.dummy.views import router
-
-__all__ = ["router"]
--- iot_backend/web/api/router.py
+++ iot_backend/web/api/router.py
@@ -4,7 +4,6 @@ from iot_backend.web.api import (  # alerts,
     actions,
     devices,
     docs,
-    dummy,
     echo,
     groups,
     monitoring,
@@ -21,7 +20,6 @@ api_router.include_router(monitoring.router)
 api_router.include_router(users.router)
 api_router.include_router(docs.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
 api_router.include_router(
     organizations.router,
